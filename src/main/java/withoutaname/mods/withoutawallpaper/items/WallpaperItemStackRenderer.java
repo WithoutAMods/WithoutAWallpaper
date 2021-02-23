260b727e3da71b66ef3e3846abc2878f410bc264
WithoutAName25 <withoutaname25@gmail.com>
2021-02-23 11:08:32 +0100

    Changed wallpaper item rendering

--- src/main/java/withoutaname/mods/withoutawallpaper/items/WallpaperItemStackRenderer.java
+++ src/main/java/withoutaname/mods/withoutawallpaper/items/WallpaperItemStackRenderer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.math.vector.Quaternion;
 import org.jetbrains.annotations.NotNull;
 import withoutaname.mods.withoutawallpaper.tools.WallpaperType;
 
@@ -37,11 +38,44 @@ public class WallpaperItemStackRenderer extends ItemStackTileEntityRenderer {
 
 		matrixStack.push();
 
-		if (p_239207_2_ != ItemCameraTransforms.TransformType.GUI) {
-			matrixStack.translate(.5, .5, .5);
-			matrixStack.scale(.5f, .5f, .5f);
-			matrixStack.translate(-.5, -.5, -.5);
+		matrixStack.translate(.5, .5, .5);
+		switch (p_239207_2_) {
+			case NONE:
+				break;
+			case THIRD_PERSON_LEFT_HAND:
+			case THIRD_PERSON_RIGHT_HAND:
+				matrixStack.translate(0d/16d, 3d/16d, 3d/16d);
+				matrixStack.rotate(new Quaternion(180, 0, 0, true));
+				matrixStack.scale(.5f, .5f, .5f);
+				break;
+			case FIRST_PERSON_LEFT_HAND:
+				matrixStack.translate(-1.13d/16d, 3.2d/16d, 1.13d/16d);
+				matrixStack.rotate(new Quaternion(0, -90, 180+25, true));
+				matrixStack.scale(.5f, .5f, .5f);
+				break;
+			case FIRST_PERSON_RIGHT_HAND:
+				matrixStack.translate(1.13d/16d, 3.2d/16d, 1.13d/16d);
+				matrixStack.rotate(new Quaternion(0, 90, 180-25, true));
+				matrixStack.scale(.5f, .5f, .5f);
+				break;
+			case HEAD:
+				matrixStack.translate(0d/16d, 6.4375d/16d, 0d/16d);
+				matrixStack.rotate(new Quaternion(90, 0, 180, true));
+				matrixStack.scale(.8f, .8f, .8f);
+				break;
+			case GUI:
+				matrixStack.rotate(new Quaternion(180, 0, 0, true));
+				break;
+			case GROUND:
+				matrixStack.rotate(new Quaternion(180, 0, 0, true));
+				matrixStack.scale(.4f, .4f, .4f);
+				break;
+			case FIXED:
+				matrixStack.translate(0d/16d, 0d/16d, 0.75d/16d);
+				matrixStack.rotate(new Quaternion(0, 0, 180, true));
+				break;
 		}
+		matrixStack.translate(-.5, -.5, -.5);
 
 		float z = p_239207_2_ == ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND || p_239207_2_ == ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND ? .75f : .5f;
 
