99dff3fed58ec1269e734bee523e7cfdfe75cfcf
WithoutAName25 <withoutaname25@gmail.com>
2021-02-25 08:03:37 +0100

    Added possibility to pick up wallpapers by pressing the middle mouse button

--- src/main/java/withoutaname/mods/withoutawallpaper/blocks/WallpaperBlock.java
+++ src/main/java/withoutaname/mods/withoutawallpaper/blocks/WallpaperBlock.java
@@ -5,18 +5,26 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.PushReaction;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockReader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import withoutaname.mods.withoutawallpaper.setup.Registration;
 import withoutaname.mods.withoutawallpaper.tools.WallpaperDesign;
+import withoutaname.mods.withoutawallpaper.tools.WallpaperType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +48,38 @@ public class WallpaperBlock extends Block {
 	@Override
 	public boolean hasTileEntity(BlockState state) {
 		return true;
+	}
+
+	@Override
+	public ItemStack getPickBlock(BlockState state, RayTraceResult target, IBlockReader world, BlockPos pos, PlayerEntity player) {
+		WallpaperType wallpaperType = null;
+		TileEntity te = world.getTileEntity(pos);
+		if (te instanceof WallpaperTile) {
+			WallpaperTile tile = (WallpaperTile) te;
+			Vector3d vec = target.getHitVec().subtract(pos.getX(), pos.getY(), pos.getZ());
+			final double d = THICKNESS * 1.01; // thickness * tolerance
+			if (tile.getType(Direction.UP).getDesign() != WallpaperDesign.NONE && 1d - vec.y < d /16d) {
+				wallpaperType = tile.getType(Direction.UP);
+			} else if (tile.getType(Direction.DOWN).getDesign() != WallpaperDesign.NONE && vec.y < d /16d) {
+				wallpaperType = tile.getType(Direction.DOWN);
+			} else if (tile.getType(Direction.NORTH).getDesign() != WallpaperDesign.NONE && vec.z < d /16d) {
+				wallpaperType = tile.getType(Direction.NORTH);
+			} else if (tile.getType(Direction.SOUTH).getDesign() != WallpaperDesign.NONE && 1d - vec.z < d /16d) {
+				wallpaperType = tile.getType(Direction.SOUTH);
+			} else if (tile.getType(Direction.EAST).getDesign() != WallpaperDesign.NONE && 1d - vec.x < d /16d) {
+				wallpaperType = tile.getType(Direction.EAST);
+			} else if (tile.getType(Direction.WEST).getDesign() != WallpaperDesign.NONE && vec.x < d /16d) {
+				wallpaperType = tile.getType(Direction.WEST);
+			}
+		}
+		if (wallpaperType != null) {
+			ItemStack itemStack = new ItemStack(Registration.WALLPAPER_ITEM.get());
+			CompoundNBT tag = itemStack.getOrCreateTag();
+			tag.put("wallpaperType", wallpaperType.toNBT());
+			return itemStack;
+		} else {
+			return super.getPickBlock(state, target, world, pos, player);
+		}
 	}
 
 	@SuppressWarnings("deprecation")
