6cab6cd2c80d1ced9fe5d219224cedbfd9036a85
WithoutAName25 <withoutaname25@gmail.com>
2021-05-19 23:10:14 +0200

    Added color selection gui background

--- src/main/java/withoutaname/mods/withoutawallpaper/gui/colorselection/ColorSelectionScreen.java
+++ src/main/java/withoutaname/mods/withoutawallpaper/gui/colorselection/ColorSelectionScreen.java
@@ -2,7 +2,10 @@ package withoutaname.mods.withoutawallpaper.gui.colorselection;
 
 import java.util.List;
 import java.util.function.Consumer;
+import javax.annotation.Nonnull;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.item.DyeColor;
@@ -13,7 +16,7 @@ import withoutaname.mods.withoutawallpaper.WithoutAWallpaper;
 
 public class ColorSelectionScreen extends Screen {
 	
-	protected final ResourceLocation GUI_TEXTURE = new ResourceLocation("textures/gui/demo_background.png");
+	protected final ResourceLocation GUI_TEXTURE = new ResourceLocation(WithoutAWallpaper.MODID, "textures/gui/color_selection.png");
 	
 	private final int xSize = 112;
 	private final int ySize = 112;
@@ -46,9 +49,25 @@ public class ColorSelectionScreen extends Screen {
 		}
 	}
 	
+	@Override
+	public void render(@Nonnull MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		this.renderBackground(matrixStack);
+		this.drawGuiBackgroundLayer(matrixStack);
+		super.render(matrixStack, mouseX, mouseY, partialTicks);
+	}
+	
 	private void onPress(DyeColor color) {
 		colorConsumer.accept(color);
 		Minecraft.getInstance().setScreen(parent);
+	}
+	
+	protected void drawGuiBackgroundLayer(MatrixStack matrixStack) {
+		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+		assert this.minecraft != null;
+		this.minecraft.getTextureManager().bind(GUI_TEXTURE);
+		int i = this.guiLeft;
+		int j = this.guiTop;
+		this.blit(matrixStack, i, j, 0, 0, this.xSize, this.ySize);
 	}
 	
 	@Override
